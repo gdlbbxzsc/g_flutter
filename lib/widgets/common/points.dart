5728b4ab615b12ddae431f01d4446b4afdc82f73
Gdl <gdlbbxzsc@163.com>
2020-07-07 14:33:52 +0800

    g

--- lib/widgets/common/points.dart
+++ lib/widgets/common/points.dart
@@ -6,27 +6,27 @@ class Point extends SizedBox {
     double size = 10,
     double borderWidth = 1,
     Color color = const Color(0xFFD6D6D6),
-    Color borderColor = const Color(0xFFD6D6D6),
+    Color borderColor,
     double topLeft = 50,
     double topRight = 50,
     double bottomLeft = 50,
     double bottomRight = 50,
   }) : super(
-          width: size,
-          height: size,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: color,
-              border: Border.all(
-                  color: borderColor,
-                  width: borderWidth,
-                  style: BorderStyle.solid),
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(topLeft),
-                  topRight: Radius.circular(topRight),
-                  bottomLeft: Radius.circular(bottomLeft),
-                  bottomRight: Radius.circular(bottomRight)),
-            ),
-          ),
-        );
+    width: size,
+    height: size,
+    child: DecoratedBox(
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(
+            color: borderColor == null ? color : borderColor,
+            width: borderWidth,
+            style: BorderStyle.solid),
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(topLeft),
+            topRight: Radius.circular(topRight),
+            bottomLeft: Radius.circular(bottomLeft),
+            bottomRight: Radius.circular(bottomRight)),
+      ),
+    ),
+  );
 }

d4cd81255b814bfaa35c768898a28a686e3c7365
Gdl <gdlbbxzsc@163.com>
2020-08-07 14:58:09 +0800

    g

---
+++ lib/widgets/common/myviews.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class Space extends SizedBox {
+  const Space(double width, double height)
+      : super(width: width, height: height);
+
+  const Space.width2() : this(2.0, 0.0);
+
+  const Space.width3() : this(3.0, 0.0);
+
+  const Space.width4() : this(4.0, 0.0);
+
+  const Space.width5() : this(5.0, 0.0);
+
+  const Space.width6() : this(6.0, 0.0);
+
+  const Space.width7() : this(7.0, 0.0);
+
+  const Space.width8() : this(8.0, 0.0);
+
+  const Space.width9() : this(9.0, 0.0);
+
+  const Space.width10() : this(10.0, 0.0);
+
+  const Space.width12() : this(12.0, 0.0);
+
+  const Space.width15() : this(15.0, 0.0);
+
+  const Space.width16() : this(16.0, 0.0);
+
+  const Space.width20() : this(20.0, 0.0);
+
+  const Space.width25() : this(25.0, 0.0);
+
+  const Space.width30() : this(30.0, 0.0);
+
+//////////////////////
+  const Space.height2() : this(0.0, 2.0);
+
+  const Space.height3() : this(0.0, 3.0);
+
+  const Space.height4() : this(0.0, 4.0);
+
+  const Space.height5() : this(0.0, 5.0);
+
+  const Space.height6() : this(0.0, 6.0);
+
+  const Space.height7() : this(0.0, 7.0);
+
+  const Space.height8() : this(0.0, 8.0);
+
+  const Space.height9() : this(0.0, 9.0);
+
+  const Space.height10() : this(0.0, 10.0);
+
+  const Space.height12() : this(0.0, 12.0);
+
+  const Space.height15() : this(0.0, 15.0);
+
+  const Space.height16() : this(0.0, 16.0);
+
+  const Space.height20() : this(0.0, 20.0);
+
+  const Space.height25() : this(0.0, 25.0);
+
+  const Space.height30() : this(0.0, 30.0);
+}
+
+class SpaceWeight extends Expanded {
+  const SpaceWeight({int flex = 1})
+      : super(flex: flex, child: const SizedBox());
+}
